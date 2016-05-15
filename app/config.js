868e0c3af06fe19180867a3ed6f3cce46479c2a5
Mikhail Vazhenin <mikhail.vazhenin@zsassociates.com>
2016-05-14 21:43:32 -0500

    Some tweaks...

--- app/config.js
+++ app/config.js
@@ -3,5 +3,7 @@ class Config {
 }
 Config.creatureBaseDamage = 10;
 Config.creatureBaseSpeed = 20;
+Config.creatureEmotions = ['Test1', 'Test2', 'Test2'];
+Config.dialogContents = ['Text1', 'Test2'];
 exports.Config = Config;
 //# sourceMappingURL=config.js.map
--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,6 @@
 export class Config {
 	static creatureBaseDamage:number = 10;
 	static creatureBaseSpeed:number = 20;
+	static creatureEmotions:string[] = ['Test1', 'Test2', 'Test2'];
+	static dialogContents:string[] = ['Text1', 'Test2'];
 }
